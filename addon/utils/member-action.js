49f6531867cff6f757ad549f0be343759a9ef221
Alexander Janssen <alexander@homeworks.nl>
2016-07-11 15:20:57 +0200

    Update actions

---
+++ addon/utils/member-action.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import buildOperationUrl from './build-url';
+
+export default function instanceOp(options) {
+  return function(payload) {
+    let modelName = this.constructor.modelName || this.constructor.typeKey;
+    let requestType = options.type || 'PUT';
+    let urlType = options.urlType || requestType;
+    let adapter = this.store.adapterFor(modelName);
+    let fullUrl = buildOperationUrl(this, options.path, urlType);
+    return adapter.ajax(fullUrl, requestType, Ember.$.extend({}, options.ajaxOptions, { data: payload }));
+  };
+}
